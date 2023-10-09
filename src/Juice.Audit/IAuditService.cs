c24bfce5e5ed53c38f13e807f42c162be5d34eca
ductran <duc@hdvietnam.com.vn>
2023-10-09 16:58:52 +0700

    Separate audit from Juice

---
+++ src/Juice.Audit/IAuditService.cs
@@ -0,0 +1,11 @@
+﻿using Juice.Audit.Domain.AccessLogAggregate;
+using Juice.Audit.Domain.DataAuditAggregate;
+
+namespace Juice.Audit
+{
+    public interface IAuditService
+    {
+        Task<IOperationResult> PersistAuditInformationAsync(AccessLog accessLog, DataAudit[] auditEntries,
+            CancellationToken token);
+    }
+}
